f5f6e9f1c76dbac4475b35497236e193c61daf0d
Jainendra <jainendra.mahajan456@gmail.com>
2025-06-03 01:57:22 +0530

    Fetch Chat from Database

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { createSocketConnection } from '../utils/socket';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
 
 
 const Chat = () => {
@@ -11,21 +13,43 @@ const Chat = () => {
     const [newMessage, setNewMessage] = useState('');
     const userId = user?._id;
     const firstName = user?.firstName;
+    const lastName = user?.lastName;
 
     useEffect(() => {
         if (!userId) return;
         const socket = createSocketConnection();
 
-        socket.emit("joinChat", { firstName, userId, targetId });
+        socket.emit("joinChat", { firstName, lastName, userId, targetId });
 
         socket.on("messageReceived", ({ firstName, text }) => {
-            setMessages(prev => [...prev, { firstName, text }]);
+            setMessages(prev => [...prev, { firstName, lastName, text }]);
         })
 
         return () => {
             socket.disconnect();
         }
     }, [userId, targetId]);
+
+
+    useEffect(() => {
+        fetchChat();
+    }, [])
+    const fetchChat = async () => {
+        const res = await axios.get(BASE_URL + `chat/${targetId}`, { withCredentials: true });
+        console.log(res?.data?.chat?.messages);
+
+
+        const chatMessages = res?.data?.chat?.messages.map(msg => {
+            const { senderId, text } = msg;
+            return {
+                firstName: senderId?.firstName,
+                lastName: senderId?.lastName,
+                text
+            }
+        });
+
+        setMessages(chatMessages);
+    }
 
 
     const sendMessage = () => {
@@ -36,14 +60,14 @@ const Chat = () => {
 
     return (
         <div className="p-4 sm:p-6 md:p-10 max-w-3xl mx-auto">
-            <h1 className="text-3xl font-semibold text-center mb-6 text-white">Chat</h1>
+            <h1 className="text-2xl font-semibold text-center text-white">Chat</h1>
 
-            <div className="mockup-window bg-base-200 border border-base-300 rounded-xl shadow-lg overflow-hidden">
+            <div className="mt-2 mockup-window bg-base-200 border border-base-300 rounded-xl shadow-lg overflow-hidden">
                 <div className="p-4 space-y-6 max-h-[60vh] overflow-y-auto">
 
 
                     {messages.map((msg, index) => (
-                        <div key={index} className="chat chat-start">
+                        <div key={index} className={"chat " + (user.firstName === msg.firstName ? "chat-end" : "chat-start")}>
                             <div className="chat-image avatar">
 
                                 <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
@@ -56,21 +80,17 @@ const Chat = () => {
 
 
                             <div className="chat-header text-white">
-                                {msg.firstName}
+                                {msg.firstName} {msg.lastName}
                                 <time className="text-xs opacity-50 ml-2">12:45</time>
                             </div>
                             <div className="chat-bubble bg-primary text-white">{msg.text}</div>
                             <div className="chat-footer text-xs opacity-60">Delivered</div>
                         </div>
                     ))}
-
-
-
-
                 </div>
             </div>
 
-            <div className="mt-4 flex items-center gap-2">
+            <div className="mt-4 flex items-center gap-2 mb-10">
                 <input
                     type="text"
                     className="input input-bordered flex-grow"
